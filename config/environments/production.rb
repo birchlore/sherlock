a66435e2b1d29e5b5341083ce2d43aeb7a034d5e
Jackson Cunningham <jacksondcunningham@gmail.com>
2015-09-02 19:02:00 -0700

    mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,17 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  config.action_mailer.delivery_method = :smtp
+    # SMTP settings for mailgun
+    ActionMailer::Base.smtp_settings = {
+      :port           => 587,
+      :address        => "smtp.mailgun.org",
+      :domain         => ENV['mailgun_domain'],
+      :user_name      => ENV['mailgun_username'],
+      :password       => ENV['mailgun_password'],
+      :authentication => :plain,
+    }}
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
