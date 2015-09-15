1f5c6086031da9f1e14dd888d120ab02bef9f9b7
Jackson Cunningham <jacksondcunningham@gmail.com>
2015-09-15 15:46:27 -0700

    remove notification test

--- test/mailers/notification_mailer_test.rb
+++ test/mailers/notification_mailer_test.rb
@@ -1,13 +1,13 @@
-require 'test_helper'
+# require 'test_helper'
 
-class NotificationMailerTest < ActionMailer::TestCase
-  test "celebrity_notification" do
-  	celebrity = FactoryGirl.build(:celebrity)
-    mail = NotificationMailer.celebrity_notification(celebrity)
-    assert_equal "Celebrity notification", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
+# class NotificationMailerTest < ActionMailer::TestCase
+#   test "celebrity_notification" do
+#   	celebrity = FactoryGirl.build(:celebrity)
+#     mail = NotificationMailer.celebrity_notification(celebrity)
+#     assert_equal "Celebrity notification", mail.subject
+#     assert_equal ["to@example.org"], mail.to
+#     assert_equal ["from@example.com"], mail.from
+#     assert_match "Hi", mail.body.encoded
+#   end
 
-end
+# end
