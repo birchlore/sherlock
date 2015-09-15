5225231f6a59b6ac2f3deebedd2ec6a07111ea7c
Jackson Cunningham <jacksondcunningham@gmail.com>
2015-09-15 15:11:59 -0700

    fix notification mailer

--- test/mailers/notification_mailer_test.rb
+++ test/mailers/notification_mailer_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class NotificationMailerTest < ActionMailer::TestCase
   test "celebrity_notification" do
-    mail = NotificationMailer.celebrity_notification
+  	celebrity = build(:celebrity)
+    mail = NotificationMailer.celebrity_notification(celebrity)
     assert_equal "Celebrity notification", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
