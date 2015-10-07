1b4ddf700fd62a0a0e5fc471f751880a0079c460
Jackson Cunningham <jacksondcunningham@gmail.com>
2015-10-07 13:14:44 -0700

    change SSL exception from .com to .co

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Groupie
     config.action_dispatch.default_headers['P3P'] = 'CP="Not used"'
     config.action_dispatch.default_headers.delete('X-Frame-Options')
     config.web_console.whitelisted_ips = '23.227.55.111'
-    config.middleware.use Rack::SslEnforcer, :only_environments => 'production', :except_hosts => ["http://www.groupie.shopify.co", "www.groupie.shopify.co"]
+    config.middleware.use Rack::SslEnforcer, :only_environments => 'production', :except_hosts => ["http://www.groupie.pixelburst.co", "www.groupie.pixelburst.co"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
